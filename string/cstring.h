2b10e47f7ce68a431911df548ae512590f4b69e2
Kurt Heiritz (pseudo) <saarthkarkera@gmail.com>
2025-04-28 19:25:10 +0530

    Update cstring.h

--- string/cstring.h
+++ string/cstring.h
@@ -12,13 +12,13 @@ typedef struct {
 String* string_create(size_t initial_cap);
 String* string_free(String* str);
 
-String* string_from_chars(char* chrs, size_t len);
-char* string_to_chars(String* str);
+String* string_from_chrs(char* chrs, size_t len);
+char* string_to_chrs(String* str);
 
-void string_push_char(String* str, char chr);
-char string_pop_char(String* str);
-void string_push_chars(String* str, char* chrs; size_t len);
-void string_pop_chars(String* str, size_t len);
+void string_push_chr(String* str, char chr);
+char string_pop_chr(String* str);
+void string_push_chrs(String* str, char* chrs; size_t len);
+void string_pop_chrs(String* str, size_t len);
 
 char string_index(String* str);
 void string_modify(String* str, char new_chr);
@@ -28,9 +28,9 @@ void string_slice_modify(String* str, char* new_chr, size_t start, size_t end);
 size_t* string_search_single(String* str, char pat);
 size_t** string_search(String* str, char* pat, size_t len);
 
-void string_add_char_at_index(String* str, char chr, size_t index);
-void string_remove_char_at_index(String* str, size_t index);
-void string_add_chars_at_index(String* str, char* chr, size_t len, size_t index);
-void string_remove_chars_at_index(String* str, size_t len, size_t index);
+void string_add_chr_at_index(String* str, char chr, size_t index);
+void string_remove_chr_at_index(String* str, size_t index);
+void string_add_chrs_at_index(String* str, char* chr, size_t len, size_t index);
+void string_remove_chrs_at_index(String* str, size_t len, size_t index);
 
 #endif
