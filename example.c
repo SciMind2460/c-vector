2fbf076b1179445a4d28d1c8bbb1dc0ec5053ca3
Kurt Heiritz (pseudo) <saarthkarkera@gmail.com>
2025-04-28 10:04:41 +0530

    Create example.c

---
+++ example.c
@@ -0,0 +1,12 @@
+#include <CVector.h>
+
+int main() {
+    Vector* vec = vector_create(100);
+    for (int i = 0; i < 100; i++) {
+        vector_push(vec, (void*) &i);
+    };
+    for (size_t i = 0; i < 100; i++) {
+        printf!("%d", vector_index(i));
+    };
+    return 0;
+}
